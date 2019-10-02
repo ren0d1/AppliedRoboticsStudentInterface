28282efe4e7b4764bc02a40d9634a26dd9c9f59a
spoonhl <hj.loeffel@web.de>
2019-10-02 17:55:19 +0200

    shows files every x sec, stores in a predefined file. Drawing and flipping works, but commented for now

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -2,21 +2,20 @@
 #include "student_planning_interface.hpp"
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <ctime>
+#include <stdexcept>
+#include <sstream>
 
 // classes which are included
 #include "ReturnPath.hpp"
 
 
-#include <ctime>
 
-
-
-#include <stdexcept>
-#include <sstream>
-
-//global variables needed to store pictures
+//global variables needed to store pictures 
 time_t now = 0;
-double elapsed_time =  10.0;
+double elapsed_time =  20.0; //time in seconds when pictures can be shown/stored.
+
+
 namespace student {
  
  void loadImage(cv::Mat& img_out, const std::string& config_folder){  
@@ -28,46 +27,49 @@ namespace student {
  void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
     
     if(now == 0 || double(time(0) - now) > elapsed_time){
-      
-      // create a path string with a time stamp
-      time_t rawtime;
-      struct tm * timeinfo;
-      char buffer[80];
-      time (&rawtime);
-      timeinfo = localtime(&rawtime);
-      strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);      
-      std::string str = "/home/hendrik/Desktop/test_images/img"; //Attention: Insert here the directory where the files should be stored.
-      std::string str1(buffer);
-      std::string str2 = ".png";
-      std::string str3 = str + str1 + str2; 
+      //update time
+      now = time(0);  
 
+      //draws a circle on the image
       //cv::circle(img_in, cv::Point (400,400), 50,  cv::Scalar (128,128,128), -1, 8);
 
-      //show a window
-      //cv::imshow("Display window", img_in);
-
-      cv::flip(img_in, img_in, 1);
-      //cv::resize(img_in, img_in, cv::Size(), 0.75, 0.75);
       
-      int x = cv::waitKey(30);
-      //safe image to path
-      bool check = cv::imwrite(str3, img_in);
 
-      // if(x == 115){
-  
-      //   bool check = cv::imwrite(str3, img_in);
-        
-      //   if(check){
-      //     throw std::logic_error( "File safed." );
-      //   }else{
-      //     throw std::logic_error( "Something went wrong." );
-      //   }
-      // } 
-      now = time(0);
+      //flips image vertically
+      //cv:: Mat img_flip;
+      //cv::flip(img_in, img_flip, 1);
 
+      //scales image
+      //cv:: Mat img_out;
+      //cv::resize(img_in,img_out , cv::Size(), 0.75, 0.75);
+
+      //show the image in a window
+      cv::imshow("Display window", img_in);
+      //waits for input, otherwise the window is open infinitely.
+      char x = cv::waitKey(0);
+
+      //enters if statement if s is pressed.
+      if(x == 's'){
+        // create a path string with a time stamp to store the image
+        time_t rawtime;
+        struct tm * timeinfo;
+        char buffer[80];
+        time (&rawtime);
+        timeinfo = localtime(&rawtime);
+        strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);      
+        std::string str = "/home/hendrik/Desktop/test_images/img"; //Attention: Insert here the directory where the files should be stored.
+        std::string str1(buffer);
+        std::string str2 = ".png";
+        std::string str3 = str + str1 + str2;
+        //stores the image
+        bool check = cv::imwrite(str3, img_in);
+      }
+      //force windows to close
+      cv::destroyAllWindows();
     }
- 
   }
+ 
+  
 
   bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
     throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );   
