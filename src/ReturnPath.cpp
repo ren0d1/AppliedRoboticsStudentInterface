8b4e0037fdeeb07fd0ff9642cc3593e65d2974e2
spoonhl <hj.loeffel@web.de>
2019-10-02 15:04:53 +0200

    test commit

---
+++ src/ReturnPath.cpp
@@ -0,0 +1,29 @@
+#include "ReturnPath.hpp"
+#include <ctime>
+//#include <string>
+
+using namespace std;
+
+std::string ReturnPath::givePath() {
+
+    // time_t rawtime;
+    // struct tm * timeinfo;
+    // char buffer[80];
+
+    // time (&rawtime);
+    // timeinfo = localtime(&rawtime);
+
+    // strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);
+
+    // std::string str = "/home/hendrik/Desktop/test_images/img"; //Attention: Insert here the directory where the files should be stored.
+    // std::string str1(buffer);
+    // std::string str2 = ".png";
+    // std::string str3 = str + str1 + str2;
+    std::string str3 = "Hello World!";
+    return str3; 
+}
+
+
+
+
+
---
+++ src/ReturnPath.hpp
@@ -0,0 +1,13 @@
+#ifndef ReturnPath_H_
+#define ReturnPath_H_
+
+
+
+class ReturnPath {
+public:
+	std::string givePath();
+	
+};
+
+
+#endif /* CAT_H_ */
